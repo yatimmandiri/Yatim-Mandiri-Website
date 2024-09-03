8c1845a121883469880e6bc5c577f4c8dec94cc7
Yatim Mandiri <scrum@yatimmandiri.org>
2024-09-03 12:04:54 +0700

    add component button
    add component button

--- src/components/layouts/MainHeader.jsx
+++ src/components/layouts/MainHeader.jsx
@@ -1,3 +1,49 @@
+import { ButtonLinkComponent } from '@/components/partials/ButtonComponent';
+import Link from 'next/link';
+
 export const MainHeader = () => {
-  return <header>MainHeader</header>;
+  const menus = [
+    {
+      label: 'Tentang Kami',
+      href: '#',
+    },
+    {
+      label: 'Layanan',
+      href: '#',
+    },
+    {
+      label: 'Program',
+      href: '#',
+    },
+    {
+      label: 'Publikasi',
+      href: '#',
+    },
+    {
+      label: 'Partnership',
+      href: '#',
+    },
+    {
+      label: 'Media',
+      href: '#',
+    },
+  ];
+
+  return (
+    <header className='flex flex-row justify-between items-center p-4 shadow'>
+      <span>Logo</span>
+      <ul className='hidden md:flex flex-row space-x-4 items-center'>
+        {menus.map((menu, i) => (
+          <Link key={i} className='font-semibold text-xs' href={menu.href}>
+            {menu.label}
+          </Link>
+        ))}
+        <ButtonLinkComponent
+          text='Donasi'
+          className='px-6 md:px-12'
+          variant='outline'
+        />
+      </ul>
+    </header>
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/utils/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
@@ -15,6 +16,21 @@ module.exports = {
           lg: '4rem',
           xl: '5rem',
           '2xl': '6rem',
+        },
+      },
+      colors: {
+        baseColor: {
+          50: '#fef5ee',
+          100: '#fde9d7',
+          200: '#fbcead',
+          300: '#f8ac79',
+          400: '#f47f43',
+          500: '#f16022',
+          600: '#e24414',
+          700: '#bb3113',
+          800: '#952817',
+          900: '#782416',
+          950: '#410f09',
         },
       },
       fontFamily: {
