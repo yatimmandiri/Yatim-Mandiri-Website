e832270ca91aaa0e19a6a451668bb2ac843b2cab
Yatim Mandiri <scrum@yatimmandiri.org>
2024-09-03 11:07:59 +0700

    main layout
    main layout

--- src/components/layouts/MainFooter.jsx
+++ src/components/layouts/MainFooter.jsx
@@ -1,34 +1,3 @@
 export const MainFooter = () => {
-  return (
-    <footer className='flex flex-col space-y-8 p-6'>
-      <ul className='flex flex-col space-y-4'>
-        <li className='mb-4'>Logo</li>
-        <li className='flex flex-col space-y-0.5'>
-          <span>Graha Yatim Mandiri</span>
-          <span>Graha Yatim Mandiri</span>
-          <span>Graha Yatim Mandiri</span>
-        </li>
-        <li className='flex flex-col space-y-0.5'>
-          <span>Graha Yatim Mandiri</span>
-          <span>Graha Yatim Mandiri</span>
-          <span>Graha Yatim Mandiri</span>
-        </li>
-      </ul>
-      <ul>
-        <li className='mb-4'>Program</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-      <ul>
-        <li className='mb-4'>Lebih Dekat</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </footer>
-  );
+  return <footer>MainFooter</footer>;
 };
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { MainFooter } from '@/components/layouts/MainFooter';
+import { MainHeader } from '@/components/layouts/MainHeader';
 import { AppProvider } from '@/hooks/useApp';
 
 export const MainLayout = ({ context, children }) => {
   return (
     <AppProvider context={context}>
-      <div>
-        <main>{children}</main>
+      <div className='flex flex-col min-h-screen'>
+        <MainHeader />
+        <main className='flex-1'>{children}</main>
         <MainFooter />
       </div>
     </AppProvider>
