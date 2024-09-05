224007db85a954e5ad1e4f9bf5678f2894fc3242
Yatim Mandiri <scrum@yatimmandiri.org>
2024-09-05 15:33:28 +0700

    Update

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,9 +1,18 @@
+import { ServiceComponent } from '@/components/sections/ServiceComponent';
 import { MainLayout } from '@/layouts/MainLayout';
+import Image from 'next/image';
 
 export default function MainPage() {
   return (
     <MainLayout>
-      <h1>MainPage</h1>
+      <Image
+        src={'https://picsum.photos/1920/1080'}
+        alt='media'
+        width={1920}
+        height={1080}
+        className='object-contain md:object-cover w-full md:h-96 md:w-full'
+      />
+      <ServiceComponent />
     </MainLayout>
   );
 }
