7f19af8c148cb5c147789630adbe6d0d46a47b5e
Yatim Mandiri <scrum@yatimmandiri.org>
2024-10-02 09:52:19 +0700

    Update ButtonComponent.jsx

--- src/components/partials/ButtonComponent.jsx
+++ src/components/partials/ButtonComponent.jsx
@@ -1,15 +1,15 @@
-import { colors } from "@/utils/colors";
-import { Button } from "@headlessui/react";
-import classNames from "classnames";
-import Link from "next/link";
-import { createElement } from "react";
+import { colors } from '@/utils/colors';
+import { Button } from '@headlessui/react';
+import classNames from 'classnames';
+import Link from 'next/link';
+import { createElement } from 'react';
 
 export const ButtonComponent = ({
-  type = "button",
-  className = "",
-  text = "",
-  color = "default",
-  variant = "solid",
+  type = 'button',
+  className = '',
+  text = '',
+  color = 'default',
+  variant = 'solid',
   block = false,
   fullWidth = false,
   pill = false,
@@ -23,38 +23,71 @@ export const ButtonComponent = ({
       type={type}
       disabled={isLoading}
       className={classNames(
-        "inline-flex space-x-2 items-center p-2 border",
-        "disabled:cursor-not-allowed",
-        pill ? "rounded-full" : "rounded-lg",
-        fullWidth ? "w-full" : "w-fit",
-        block ? "justify-between" : "justify-center",
+        'inline-flex space-x-2 items-center p-2 border',
+        'disabled:cursor-not-allowed',
+        pill ? 'rounded-full' : 'rounded-lg',
+        fullWidth ? 'w-full' : 'w-fit',
+        block ? 'justify-between' : 'justify-center',
         colors[variant][color],
         className
       )}
       {...props}
     >
-      {leftIcon && createElement(leftIcon, { className: "w-4 h-4" })}
-      <span className="text-xs font-semibold">
-        {isLoading ? "Loading..." : text}
+      {leftIcon && createElement(leftIcon, { className: 'w-4 h-4' })}
+      <span className='text-xs font-semibold'>
+        {isLoading ? 'Loading...' : text}
       </span>
-      {rightIcon && createElement(rightIcon, { className: "w-4 h-4" })}
+      {rightIcon && createElement(rightIcon, { className: 'w-4 h-4' })}
+    </Button>
+  );
+};
+
+export const ButtonIconComponent = ({
+  type = 'button',
+  className = '',
+  text = '',
+  color = 'default',
+  variant = 'solid',
+  block = false,
+  fullWidth = false,
+  pill = false,
+  isLoading = false,
+  icons = false,
+  ...props
+}) => {
+  return (
+    <Button
+      type={type}
+      disabled={isLoading}
+      className={classNames(
+        'inline-flex space-x-2 items-center p-2 border',
+        'disabled:cursor-not-allowed',
+        pill ? 'rounded-full' : 'rounded-lg',
+        fullWidth ? 'w-full' : 'w-fit',
+        block ? 'justify-between' : 'justify-center',
+        colors[variant][color],
+        className
+      )}
+      {...props}
+    >
+      {icons && createElement(icons, { className: 'w-4 h-4' })}
     </Button>
   );
 };
 
 export const ButtonLinkComponent = ({
-  type = "button",
-  className = "",
-  text = "",
-  color = "default",
-  variant = "solid",
+  type = 'button',
+  className = '',
+  text = '',
+  color = 'default',
+  variant = 'solid',
   block = false,
   fullWidth = false,
   pill = false,
   isLoading = false,
   leftIcon = false,
   rightIcon = false,
-  href = "#",
+  href = '#',
   funct,
   ...props
 }) => {
@@ -66,21 +99,21 @@ export const ButtonLinkComponent = ({
       type={type}
       disabled={isLoading}
       className={classNames(
-        "inline-flex space-x-2 items-center p-2 border",
-        "disabled:cursor-not-allowed",
-        pill ? "rounded-full" : "rounded-lg",
-        fullWidth ? "w-full" : "w-fit",
-        block ? "justify-between" : "justify-center",
+        'inline-flex space-x-2 items-center p-2 border',
+        'disabled:cursor-not-allowed',
+        pill ? 'rounded-full' : 'rounded-lg',
+        fullWidth ? 'w-full' : 'w-fit',
+        block ? 'justify-between' : 'justify-center',
         colors[variant][color],
         className
       )}
       {...props}
     >
-      {leftIcon && createElement(leftIcon, { className: "w-4 h-4" })}
-      <span className="text-xs font-semibold">
-        {isLoading ? "Loading..." : text}
+      {leftIcon && createElement(leftIcon, { className: 'w-4 h-4' })}
+      <span className='text-xs font-semibold'>
+        {isLoading ? 'Loading...' : text}
       </span>
-      {rightIcon && createElement(rightIcon, { className: "w-4 h-4" })}
+      {rightIcon && createElement(rightIcon, { className: 'w-4 h-4' })}
     </Button>
   );
 };
