7e1acdd6d9bd9418b50c2e47acf297a5613ecd99
Yatim Mandiri <scrum@yatimmandiri.org>
2024-10-02 09:58:50 +0700

    Update MainHeader.jsx

--- src/components/layouts/MainHeader.jsx
+++ src/components/layouts/MainHeader.jsx
@@ -1,3 +1,50 @@
+import { ButtonLinkComponent } from '@/components/partials/ButtonComponent';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export const MainHeader = () => {
-  return <div>MainHeader</div>;
+  const menus = [
+    {
+      label: 'Tentang Kami',
+      href: '#',
+    },
+    {
+      label: 'Layanan',
+      href: '#',
+    },
+    {
+      label: 'Program',
+      href: '#',
+    },
+    {
+      label: 'Publikasi',
+      href: '#',
+    },
+    {
+      label: 'Partnership',
+      href: '#',
+    },
+    {
+      label: 'Media',
+      href: '#',
+    },
+  ];
+
+  return (
+    <header className='flex flex-row justify-between items-center p-4 px-12 shadow'>
+      <Image src='img/ym-logo.svg' alt='Logo' width={40} height={40} />
+      <ul className='hidden md:flex flex-row space-x-7 items-center'>
+        {menus.map((menu, i) => (
+          <Link
+            key={i}
+            className='font-semibold text-xs hover:text-baseColor-600'
+            href={menu.href}
+          >
+            {menu.label}
+          </Link>
+        ))}
+        <ButtonLinkComponent text='Donasi' className='px-6 md:px-12' />
+      </ul>
+    </header>
+  );
 };
